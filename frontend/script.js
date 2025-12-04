d4f6087b29fa15a7217fa580cb064499c074b087
Håkon Vågsether <hakon.vagsether@gmail.com>
2025-12-04 20:18:14 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,3 @@
-async function callCppServer() {
-    const response = await fetch('http://localhost:8080/rpc/add', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ x: 10, y: 20 })
-    });
-
-    const data = await response.json();
-    console.log("Result from C++:", data.result);
-}
-
 async function callBlockCount() {
     const response = await fetch('http://localhost:8080/blocks/count', {
         method: 'POST',
@@ -23,3 +10,4 @@ async function callBlockCount() {
     const data = await response.json();
     console.log("Result from C++:", data.result);
 }
+
