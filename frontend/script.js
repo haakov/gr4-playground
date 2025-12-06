47fa34387ae136823faa8c547308dd2dcc972bc9
Håkon Vågsether <hakon.vagsether@gmail.com>
2025-12-06 11:42:03 +0100

    Update frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
-async function callBlockCount() {
-    const response = await fetch('http://localhost:8080/blocks/count', {
+async function blockCount() {
+    const response = await fetch('http://localhost:8080/block_count', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -11,3 +11,15 @@ async function callBlockCount() {
     console.log("Result from C++:", data.result);
 }
 
+async function blocks() {
+    const response = await fetch('http://localhost:8080/blocks', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    });
+
+    const data = await response.json();
+    console.log("Result from C++:", data.blocks);
+}
