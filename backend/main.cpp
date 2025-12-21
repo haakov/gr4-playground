8d8ee67da668f8f7022a157c95770b6800e4d9d4
Håkon Vågsether <hakon.vagsether@gmail.com>
2025-12-21 21:08:33 +0100

    Add block_info

--- backend/main.cpp
+++ backend/main.cpp
@@ -71,6 +71,54 @@ int main() {
         res.set_content(j.dump(), "application/json");
     });
 
+    svr.Options("/block_info", [](const httplib::Request& req, httplib::Response& res) {
+        enableCORS(res);
+        res.status = 204;
+    });
+    svr.Post("/block_info", [&registry](const httplib::Request& req, httplib::Response& res) {
+        enableCORS(res);
+        json data = json::parse(req.body);
+
+        json p_;
+        std::string block = data["block"];
+        std::println("Block info {}", block);
+
+        if (!registry->contains(block)) {
+            p_["error"] = "No such block";
+            p_["parameters"] = {};
+            p_["inputs"] = {};
+            p_["outputs"] = {};
+            res.set_content(p_.dump(), "application/json");
+            std::println("No such block in registry");
+            return;
+        }
+
+        // Block exists
+
+        gr::property_map map_;
+        auto block_mod = registry->create(block, map_);
+        block_mod->settings().init();
+        std::vector<std::string> parameters;
+        std::vector<std::tuple<std::string,std::string>> inputs, outputs;
+        for (const auto& [key, value] : block_mod->settings().defaultParameters()) {
+            parameters.push_back(key);
+        }
+        auto out = block_mod->outputMetaInfos();
+        auto in = block_mod->inputMetaInfos();
+        for (const auto& item : in) {
+            inputs.push_back(std::tuple<std::string,std::string>(item.signal_name.value, item.data_type.value));
+        }
+        for (const auto& item : out) {
+            outputs.push_back(std::tuple<std::string,std::string>(item.signal_name.value, item.data_type.value));
+        }
+
+        p_["parameters"] = parameters;
+        p_["inputs"] = inputs;
+        p_["outputs"] = outputs;
+
+        res.set_content(p_.dump(), "application/json");
+    });
+
     std::cout << "Server started at localhost:8080" << std::endl;
     svr.listen("0.0.0.0", 8080);
 
--- frontend/script.js
+++ frontend/script.js
@@ -23,3 +23,18 @@ async function blocks() {
     const data = await response.json();
     console.log("Result from C++:", data.blocks);
 }
+
+async function blockInfo() {
+    const response = await fetch('http://localhost:8080/block_info', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"block": "gr::basic::FunctionGenerator<float32>"})
+    });
+
+    const data = await response.json();
+    console.log("Block parameters:", data.parameters);
+    console.log("Block input ports:", data.inputs);
+    console.log("Block output ports:", data.outputs);
+}
